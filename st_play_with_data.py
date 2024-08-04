ecf84b58854155301d07dc0107031762aa6e0bef
Rahul Soundrarajan <rahulsound@gmail.com>
2024-08-04 13:50:34 +0530

    Used Px in place of sns.

--- st_play_with_data.py
+++ st_play_with_data.py
@@ -3,6 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("whitegrid")
+import plotly.express as px
+import plotly.figure_factory as ff
+
+
+plt.rcParams['figure.figsize'] = [4, 4]
 
 
 def run_play_with_data():
@@ -25,28 +30,31 @@ def run_play_with_data():
             ''', language='python') 
     feature_x = st.selectbox('Select feature for x axis', c1.columns)
     feature_y = st.selectbox('Select feature for y axis', c1.columns)   
+    feature_z = st.selectbox('Select the split-by criterion: [slice, agent]', ['slice', 'agent'])
 
-    fig, ax = plt.subplots()
-    sns.scatterplot(data=c1, x=feature_x, y=feature_y, ax=ax)
-    st.pyplot(fig)
+    fig, ax = plt.subplots(figsize=(4, 4))
+    # sns.scatterplot(data=c1, x=feature_x, y=feature_y, ax=ax)
+    # st.pyplot(fig)
+    scatter = px.scatter(data_frame=c1, x=feature_x, y=feature_y, color=feature_z)
+    st.plotly_chart(scatter)
     st.divider()
 
-    st.subheader("Plot of Distribution of KPIs per agent OR per slice")
-    st.code(''' 
-            #Description of columns:
-            'count_norm': count of repeats of a particular state [2 means the state has repeated twice]
-            '{kpi}_norm': normalized value of the KPIs: 'tx_brate', 'tx_pkts', 'dl_buffer'
-            'slice'     : slice IDs: [0,1,2]
-            'agent'     : agents: ['embb-trf1', 'embb-trf2', 'urllc-trf1', 'urllc-trf1'  ]
-            ''', language='python') 
+    # st.subheader("Plot of Distribution of KPIs per agent OR per slice")
+    # st.code(''' 
+    #         #Description of columns:
+    #         'count_norm': count of repeats of a particular state [2 means the state has repeated twice]
+    #         '{kpi}_norm': normalized value of the KPIs: 'tx_brate', 'tx_pkts', 'dl_buffer'
+    #         'slice'     : slice IDs: [0,1,2]
+    #         'agent'     : agents: ['embb-trf1', 'embb-trf2', 'urllc-trf1', 'urllc-trf1'  ]
+    #         ''', language='python') 
     
 
-    feature_x = st.selectbox('Select KPI', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
-    feature_y = st.selectbox('Select Split-by cretieron: ', ['slice', 'agent'])
+    # feature_x = st.selectbox('Select KPI', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
+    # feature_y = st.selectbox('Select Split-by cretieron: ', ['slice', 'agent'])
 
-    distplot = sns.displot(data=c1, x=feature_x, hue=feature_y, kind="kde")
-    st.pyplot(distplot.fig)
-    st.divider()  
+    # distplot = sns.displot(data=c1, x=feature_x, hue=feature_y, kind="kde")
+    # st.pyplot(distplot.fig)
+    # st.divider()  
 
     st.subheader("Plot of Distribution of KPIs per agent OR per slice  | side-by-side view:")
     st.code(''' 
@@ -70,20 +78,23 @@ def run_play_with_data():
     st.pyplot(distplot1.fig)
     st.divider()   
 
-    # st.subheader("KDE Plot of Distribution of KPIs per agent OR per slice")
-    # st.code(''' 
-    #         #Description of columns:
-    #         'count_norm': count of repeats of a particular state [2 means the state has repeated twice]
-    #         '{kpi}_norm': normalized value of the KPIs: 'tx_brate', 'tx_pkts', 'dl_buffer'
-    #         'slice'     : slice IDs: [0,1,2]
-    #         'agent'     : agents: ['embb-trf1', 'embb-trf2', 'urllc-trf1', 'urllc-trf1'  ]
-    #         ''', language='python') 
+    st.subheader(" Plot of Distribution of KPIs per agent OR per slice")
+    st.code(''' 
+            #Description of columns:
+            'count_norm': count of repeats of a particular state [2 means the state has repeated twice]
+            '{kpi}_norm': normalized value of the KPIs: 'tx_brate', 'tx_pkts', 'dl_buffer'
+            'slice'     : slice IDs: [0,1,2]
+            'agent'     : agents: ['embb-trf1', 'embb-trf2', 'urllc-trf1', 'urllc-trf1'  ]
+            ''', language='python') 
     
-    # plt.clf()
-    # feature_x2 = st.selectbox('Select KPI for KDE plot', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
-    # feature_y2 = st.selectbox('Select Split-by cretieron for KDE plot: ', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
+    plt.clf()
+    feature_x2 = st.selectbox('Select KPI ', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
+    # feature_y2 = st.selectbox('Select Split-by cretieron for KDE plot: ', ['slice', 'agent'])
+    feature_z2 = st.selectbox('Select 2nd order split-by criterion', ['slice', 'agent'])
 
-    # kdeplot = sns.kdeplot(data=c1, x=feature_x2, y=feature_y2, hue='agent', col='slice')
-    # st.pyplot(kdeplot.figure.figure)
-
-    # st.divider()  
+    kdeplot = px.histogram(data_frame=c1, x=feature_x2, color=feature_z2, histnorm='density')
+    st.plotly_chart(kdeplot)
+    kdeplot = px.density_heatmap(data_frame=c1, x=feature_x2, y=feature_z2)
+    st.plotly_chart(kdeplot)
+    
+    st.divider()  
