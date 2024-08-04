f8ce5dfd7ff9b95318ddc821cefe647de563132e
Rahul Soundrarajan <rahulsound@gmail.com>
2024-08-04 11:56:26 +0530

    Viz.

--- st_play_with_data.py
+++ st_play_with_data.py
@@ -12,7 +12,7 @@ def run_play_with_data():
     c1 = combo[[ 'count_norm', 'tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm', 'slice', 'agent']]
     dfx = c1['agent'].value_counts()
     st.subheader('Count of samples per agent: ')
-    st.bar_chart(dfx)
+    st.bar_chart(dfx, width=400)
     st.divider()
 
     st.subheader("Correlation Analysis of 2 variables")
@@ -69,3 +69,21 @@ def run_play_with_data():
     distplot1 = sns.displot(data=c1, x=feature_x1, hue=feature_y1, col=col, kind="kde")
     st.pyplot(distplot1.fig)
     st.divider()   
+
+    # st.subheader("KDE Plot of Distribution of KPIs per agent OR per slice")
+    # st.code(''' 
+    #         #Description of columns:
+    #         'count_norm': count of repeats of a particular state [2 means the state has repeated twice]
+    #         '{kpi}_norm': normalized value of the KPIs: 'tx_brate', 'tx_pkts', 'dl_buffer'
+    #         'slice'     : slice IDs: [0,1,2]
+    #         'agent'     : agents: ['embb-trf1', 'embb-trf2', 'urllc-trf1', 'urllc-trf1'  ]
+    #         ''', language='python') 
+    
+    # plt.clf()
+    # feature_x2 = st.selectbox('Select KPI for KDE plot', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
+    # feature_y2 = st.selectbox('Select Split-by cretieron for KDE plot: ', ['tx_brate_norm', 'tx_pkts_norm', 'dl_buffer_norm'])
+
+    # kdeplot = sns.kdeplot(data=c1, x=feature_x2, y=feature_y2, hue='agent', col='slice')
+    # st.pyplot(kdeplot.figure.figure)
+
+    # st.divider()  
